338de017cb09becab3b7f7605f62b434d5f236b1
VinylmastersLtd <vinylmastersltd@gmail.com>
2023-08-07 12:54:44 +0100

    s

---
+++ pages/specificPage.js
@@ -0,0 +1,16 @@
+// pages/specificPage.js
+
+import React from 'react';
+import CustomLayout from '../components/customLayout';
+
+const SpecificPage = () => {
+  return (
+    <CustomLayout>
+      {he}
+      <h1>This is the specific page content</h1>
+      <p>More content goes here...</p>
+    </CustomLayout>
+  );
+};
+
+export default SpecificPage;
