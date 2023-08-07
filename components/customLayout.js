e4852d125cc04b919fa78c593a0add7697fd6e36
VinylmastersLtd <vinylmastersltd@gmail.com>
2023-08-07 13:06:39 +0100

    Update customLayout.js

--- components/customLayout.js
+++ components/customLayout.js
@@ -1,7 +1,7 @@
 // components/customLayout.js
 
 import React from 'react';
-import Sidebar from '/sidebar';
+import Sidebar from 'components/sidebar.js';
 
 const CustomLayout = ({ children }) => {
   return (
